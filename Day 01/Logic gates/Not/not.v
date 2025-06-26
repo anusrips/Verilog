c3de7561e8dde6fbf7d82b1b526a039c165f5093
anusrips <anusrisaravananps@gmail.com>
2025-06-26 19:17:06 +0530

    Update not.v

--- Day 01/Logic gates/Not/not.v
+++ Day 01/Logic gates/Not/not.v
@@ -1,1 +1,20 @@
+design
 
+module not_gate_b(input a,output y);
+  assign y=~a;
+endmodule  
+
+testbench
+module not_gate_tb;
+  reg a;
+  wire y;
+  not_gate_b dut(.a(a),.y(y));
+  initial begin
+    $dumpfile("waves.vcd");
+    $dumpvars();
+    $monitor("a=%b,y=%b",a,b);
+  end
+  initial begin
+    #10 a=1;
+  end
+endmodule
